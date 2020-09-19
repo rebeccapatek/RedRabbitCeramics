e1ec26dd9d5de4f1b9b874476f5d321b02c0fbcc
rebeccapatek <rebeccapatek@gmail.com>
2020-09-19 10:14:19 -0500

    added some items to sell in store

---
+++ scripts/CeramicsDataProvider.js
@@ -0,0 +1,46 @@
+const ceramicsCollection = [
+    {
+        title: "Red Rabbit Mug",
+        type: "mug",
+        price: 25,
+        images: ["img1.img", "img2.img", "img3.img"],
+        sold: false
+
+    },
+    {
+        title: "Ant-Eater Mug",
+        type: "mug",
+        price: 25,
+        images: ["img1.img", "img2.img", "img3.img"],
+        sold: false
+
+    },
+    {
+        title: "Elaphant Mug",
+        type: "mug",
+        price: 25,
+        images: ["img1.img", "img2.img", "img3.img"],
+        sold: false
+    },
+    {
+        title: "Red Rabbit planter",
+        type: "planter",
+        price: 30,
+        images: ["img1.img", "img2.img", "img3.img"],
+        sold: false
+    },
+    {
+        title: "yellow stripe planter",
+        type: "planter",
+        price: 20,
+        images: ["img1.img", "img2.img", "img3.img"],
+        sold: false
+    },
+    {
+        title: "Coffee Pourover and carafe",
+        type: "Coffee-maker",
+        price: 45,
+        images: ["img1.img", "img2.img", "img3.img"],
+        sold: false
+    }
+]
