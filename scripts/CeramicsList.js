dff2a939ee8ddbd16e39b6bf7933d88de15a615b
rebeccapatek <rebeccapatek@gmail.com>
2020-09-22 16:32:41 -0500

    console logging pottery objects

--- scripts/CeramicsList.js
+++ scripts/CeramicsList.js
@@ -7,7 +7,8 @@ export const CeramicsList = () => {
     const contentElement = document.querySelector("#container")
     const ceramics = useCeramics()
     let potteryHTMLRepresentations = ""
-    for (const pot of Ceramics) {
+    for (const pot of ceramics) {
+        console.log(pot)
 
     }
 
