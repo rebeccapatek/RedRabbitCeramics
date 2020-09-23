a7d544c6ef3ae0d142df2961e5ddcaeada0c364d
rebeccapatek <rebeccapatek@gmail.com>
2020-09-23 16:51:42 -0500

    took some extra code out of main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,7 @@ import { CeramicsList } from './CeramicsList.js'
 
 const allTheCeramics = useCeramics()
 
-for (const ceramics of allTheCeramics) {
-    console.log(ceramics)
-}
+
 console.log("Welcome to the main module")
 CeramicsList();
 
