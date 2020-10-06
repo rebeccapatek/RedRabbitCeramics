50bb466fc08617b7d6cc7c5005ef341ed1dca9b4
rebeccapatek <rebeccapatek@gmail.com>
2020-10-06 16:03:08 -0500

    got products to show up again

--- scripts/CeramicsList.js
+++ scripts/CeramicsList.js
@@ -1,10 +1,34 @@
 import { useCeramics } from './CeramicsDataProvider.js'
 import { CeramicsComponent } from './Ceramics.js'
+const eventHub = document.querySelector(".container")
+let type = null;
+
+
+// eventHub.addEventListener("ceramicTypeChosen", event => {
+//     // Use the property you added to the event detail.
+//     if (event.detail.ceramicTypeThatWasChosen !== "0") {
+//         type = event.detail.ceramicTypeThatWasChosen
+//         /*
+//             Filter the criminals application state down to the people that committed the crime
+//         */
+//         const matchingCeramic = appStateCeramics.filter()
+
+//         /*
+//             Then invoke render() and pass the filtered collection as
+//             an argument
+//         */
+//     }
+// })
+
+// const render = criminalCollection => {
+//     contentTarget.innerHTML = you_fill_this_in
+// }
+
 
 export const CeramicsList = () => {
 
     // Get a reference to the `<article class="content">` element
-    const contentElement = document.querySelector("store")
+    const contentElement = document.querySelector(".store")
     const pots = useCeramics()
     let potteryHTMLRepresentations = ""
     for (const pot of pots) {
