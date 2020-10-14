66fb868bbd9c2241d8ee789cdead17434a4065b1
rebeccapatek <rebeccapatek@gmail.com>
2020-10-14 13:56:23 -0500

    tried changing the scope of the eventHub

--- scripts/CeramicsSorter.js
+++ scripts/CeramicsSorter.js
@@ -2,6 +2,7 @@ export const eventHub = document.querySelector(".container")
 
 export const CeramicsSorter = () => {
     const contentElement = document.querySelector(".sorter")
+
     contentElement.innerHTML += `
     <select id="ceramicTypeSelect">
         <option value="0">Sort By</option>
@@ -10,28 +11,30 @@ export const CeramicsSorter = () => {
         <option value="Coffee-maker">Coffee-Making Accesories</option>
     </select>
     `
+    eventHub.addEventListener('ceramicTypeChosen', changeEvent => {
+
+
+
+        if (changeEvent.target.id === 'ceramicTypeSelect') {
+
+            console.log("message was sent to Event Hub");
+            // Create custom event. Provide an appropriate name.
+            const customEvent = new CustomEvent('ceramicTypeChosen', {
+                detail: {
+                    ceramicTypeThatWasChosen: changeEvent.target.value
+                },
+
+            }
+            )
+
+            // Dispatch to event hub
+            eventHub.dispatchEvent(customEvent)
+
+        }
+    });
+
 
 }
 
-eventHub.addEventListener('ceramicTypeChosen', changeEvent => {
 
-
-
-    if (changeEvent.target.id === 'ceramicTypeSelect') {
-
-        console.log("message was sent to Event Hub");
-        // Create custom event. Provide an appropriate name.
-        const customEvent = new CustomEvent('ceramicTypeChosen', {
-            detail: {
-                ceramicTypeThatWasChosen: changeEvent.target.value
-            },
-
-        }
-        )
-
-        // Dispatch to event hub
-        eventHub.dispatchEvent(customEvent)
-
-    }
-})
 
